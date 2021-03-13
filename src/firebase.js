8e1ac5fe607533a468b495513d6b1e755b052b73
maurorpo <morerpo@gmail.com>
2021-03-13 07:34:47 -0500

    Create proyect - Firebase Implementation

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBB9Ql_DB46HQsv0twxljNvfMV8--rb92s",
+  authDomain: "facebook-clone-ceba2.firebaseapp.com",
+  projectId: "facebook-clone-ceba2",
+  storageBucket: "facebook-clone-ceba2.appspot.com",
+  messagingSenderId: "927162764124",
+  appId: "1:927162764124:web:01ef8061954f2f22b48f99",
+  measurementId: "G-YE5YEX2NGG"
+}; 
